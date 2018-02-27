8c9b7d7ede7f3b93276ffe839a01e1087baa39a8
monish0106 <keswanimonish@yahoo.com>
2018-02-28 02:31:12 +0530

    Update generateSentences.py

--- generateSentences.py
+++ generateSentences.py
@@ -4,7 +4,6 @@ import random as random
 import numpy
 
 dir_name = 'gutenberg'
-d = 0.9
 
 fp = open(dir_name+'/'+'bigram_dict','r')
 bigram_dict = pickle.load(fp)
@@ -24,22 +23,15 @@ for i in range(0,10):
     
     denom = sum(bigram_dict[word1].values())
     len1 =  len(bigram_dict[word1].keys())
+    list1 = bigram_dict[word1].keys()
     
-    prob_dist = []
+    prob_dist = [x /denom for x in bigram_dict[word1].values()]  
+    k = numpy.random.choice(list1,1,prob_dist)
 
-    for word in unigram_dict.keys():
-        
-        if bigram_dict[word1][word] !=0:
-            prob_dist.append((bigram_dict[word1][word] -d )/ denom)
-        else:        
-            prob_dist.append(d *len1 / ((total_length -len1) * denom)) 
-    
-    k = numpy.random.choice(unigram_dict.keys(),1,prob_dist)
-    
     word = "".join(k)
     
-    while word == 'UNKNOWN':
-        k = numpy.random.choice(unigram_dict.keys(),1,prob_dist)
+    while word == 'UNKNOWN' or word =='STOP1' or word == 'STOP2' or word =='START1' or word =='START2' :
+        k = numpy.random.choice(list1,1,prob_dist)
         word = "".join(k)
     
     word1 = word 
